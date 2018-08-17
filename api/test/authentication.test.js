986cddfd83629b11a5a011607119521433b8bc48
Dork <omgdork@yahoo.com>
2018-08-17 16:32:46 +0800

    Added initial tests for API.

---
+++ api/test/authentication.test.js
@@ -0,0 +1,15 @@
+describe('Authentication', () => {
+  describe('/login', () => {
+    it('should not be authorized when there is no username or password.', () => {
+      
+    });
+
+    it('should not be authorized when the username and password do not match.', () => {
+      
+    });
+
+    it('should be authorized when the username and password match.', () => {
+
+    });
+  });
+});
---
+++ api/test/medications.test.js
@@ -0,0 +1,19 @@
+describe('Medications', () => {
+  describe('/search_medication', () => {
+    it('should throw an error if medication is not provided.', () => {
+
+    });
+
+    it('should throw an error if authentication token is not provided.', () => {
+
+    });
+
+    it('should send a list of medications if there are any.', () => {
+
+    });
+
+    it('should send error details when an item is not found.', () => {
+
+    });
+  });
+});
